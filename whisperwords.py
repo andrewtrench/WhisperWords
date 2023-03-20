56648e0704c8908c7f678a956373a5b3d6f4e7a6
Andrew Trench <andrewjohntrench@gmail.com>
2023-03-20 17:57:01 +0200

    trying to get api key to load

--- whisperwords.py
+++ whisperwords.py
@@ -9,10 +9,11 @@ import streamlit as st
 from dotenv import load_dotenv
 import streamlit as st
 
-#load_dotenv()
-dotenv.find_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+st.write(os.environ)
 
 # global variables
 
