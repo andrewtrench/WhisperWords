3caea0f03f78c4a8b51ad0d1f21abb45749be964
Andrew Trench <andrewjohntrench@gmail.com>
2023-03-21 09:34:13 +0200

    handling for larger files

--- whisperwords.py
+++ whisperwords.py
@@ -3,10 +3,10 @@
 
 import os
 import re
-
+from pydub import AudioSegment
 import openai
 import streamlit as st
-
+import ffmpeg
 import streamlit as st
 
 openai.api_key = st.secrets['OPENAI_API_KEY']
@@ -16,8 +16,8 @@ openai.api_key = st.secrets['OPENAI_API_KEY']
 
 def id_questions(text):
     pattern = r"([^.!?]*\?)"
-    new_text = re.sub(pattern, r"\n\n\1<strong>", text)
-    new_text = new_text.replace("?", "?\n\n</strong>")
+    new_text = re.sub(pattern, r"\n\n\1</strong>", text)
+    new_text = new_text.replace("?", "?\n\n<strong>")
     return new_text
 
 
@@ -34,6 +34,18 @@ def upload_file():
         # Write the file to the uploads folder
         with open(filepath, "wb") as f:
             f.write(file.getbuffer())
+        # if file greater than 24mb split it into 1mb chunks using ffmpeg
+        filesize = os.path.getsize(filepath)/(1024*1024)
+        if filesize > 24:
+            os.mkdir("chunks")
+            chunk_size = 1024*1024
+            input_file_size = os.path.getsize(filepath)
+            audio = AudioSegment.from_file(filepath)
+            for i,chunk in enumerate(chunks):
+                chunk.export(f"chunks/{filename}_{i}.wav", format="wav")
+            filepath = os.listdir("chunks")
+            return filepath
+
         # Print a success message
         st.success(f"File saved to {filepath}")
         return filepath
@@ -41,18 +53,27 @@ def upload_file():
 
 def _transcribe(audio_path: str):
     """Transcribe the audio file using whisper"""
-    if audio_path:
+    if type(audio_path) == list:
+        for audio in audio_path:
+            audio_file = open(audio_path, "rb")
+            transcript = openai.Audio.transcribe("whisper-1", audio_file,
+                                                 response="verbose_json",
+                                                 temperature=0.5, )
+            text = id_questions(transcript['text'])
+            st.markdown(text, unsafe_allow_html=True)
+
+    else:
         audio_file = open(audio_path, "rb")
-        transcript = openai.Audio.translate("whisper-1", audio_file,
+        transcript = openai.Audio.transcribe("whisper-1", audio_file,
                                             response="verbose_json",
                                             temperature=0.5, )
         text = id_questions(transcript['text'])
         st.markdown(text,unsafe_allow_html=True)
-        return transcript
 
 
 if __name__ == "__main__":
     st.title("Whisper UI")
     filepath = upload_file()
-
-    _transcribe(filepath)
+    transcribe_button = st.button("Transcribe")
+    if transcribe_button:
+        _transcribe(filepath)
